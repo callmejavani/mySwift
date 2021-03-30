249495ac396f1c7bb54201bc75e92a9c381134d7
MacBook Pro <macbookpro@MacBook-Pro-MacBook.local>
2021-03-30 14:50:33 +0300

    VKApp1_ArtemVorobyev

---
+++ vkApp/vkApp/Controller/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  vkApp
+//
+//  Created by MacBook Pro on 30.03.2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var password: UILabel!
+    @IBOutlet weak var userNametextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
+    @IBAction func loginButton(_ sender: Any) {
+        
+    }
+
+
+}
+
